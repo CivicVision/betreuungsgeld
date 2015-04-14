c38fb0106b0fbd87d54739c2547377e62e7f3995
mila.frerichs@gmail.com <mila.frerichs@gmail.com>
2015-04-14 11:58:08 +0200

    Automated commit at 2015-04-14 09:58:08 UTC by middleman-deploy 1.0.0

---
+++ javascripts/map.js
@@ -0,0 +1,6 @@
+(function() {
+  $(function() {
+    return cartodb.createVis('map', 'https://milafrerichs.cartodb.com/api/v2/viz/5a0357be-e287-11e4-8378-0e018d66dc29/viz.json');
+  });
+
+}).call(this);
