b3c50054517ca6d3f50975afa15bc56035c10f86
Mila Frerichs <mila.frerichs@gmail.com>
2015-04-14 11:56:55 +0200

    First version of viz with cartodb

---
+++ source/javascripts/all.js
@@ -0,0 +1,7 @@
+//= require d3
+//= require queue-async
+//= require colorbrewer
+//= require underscore
+//= require jquery
+//= require d3graph
+//= require_tree .
---
+++ source/javascripts/map.coffee
@@ -0,0 +1,2 @@
+$ ->
+	cartodb.createVis('map','https://milafrerichs.cartodb.com/api/v2/viz/5a0357be-e287-11e4-8378-0e018d66dc29/viz.json')
